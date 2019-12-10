7b275a4ffc50b44e8fedced2a5282022afb1b552
carr-jar <1773298425@qq.com>
2019-12-10 13:26:41 +0800

    初始化

---
+++ src/main/java/com/zyp/mapper/UserMapper.java
@@ -0,0 +1,5 @@
+package com.zyp.mapper;
+
+public interface UserMapper {
+
+}
---
+++ src/main/java/com/zyp/service/imp/UserServiceImp.java
@@ -0,0 +1,9 @@
+package com.zyp.service.imp;
+
+import org.springframework.stereotype.Service;
+
+import com.zyp.service.UserService;
+@Service
+public class UserServiceImp implements UserService {
+	
+}
